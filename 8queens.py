8fc99afc1ab6d410bfe886d3a7e917fb8c970d14
t1mel0rd42 <dazk2009@gmail.com>
2022-02-13 22:13:58 -0800

    Final v1.0

--- 8queens.py
+++ 8queens.py
@@ -4,32 +4,35 @@ import multiprocessing as mp
 import time
 
 QUEENS = 8
+CANDIDATE_COUNT = 50
+MUTAGEN = 1/(QUEENS/2)
+REPORT_FREQUENCY = 1
+TRIALS = 1
+SHOW_BOARD = True
+OUTPUT = False
+
 BOARD = [['_' for row in range(QUEENS)] for col in range(QUEENS)]       
 BEST_FITNESS = int((QUEENS * (QUEENS-1)) / 2)
-CANDIDATE_COUNT = 10
-MUTAGEN = 0.25
-REPORT_FREQUENCY = 1000
-TRIALS = 250
+#ODD_OR_EVEN = sum(range(QUEENS)) % 2 # 0 if even, 1 if odd
 
+# Map of CANDIDATE_COUNTS to be used in a trial
 candidate_count_list = {
-    0 : 50,
-    1 : 75,
-    2 : 100,
-    3 : 150,
-    4 : 250,
-    5 : 500,
-#    6 : 1000,
-#    7 : 1250,
-#    8 : 1500,
-#    9 : 1750,
-#    10 : 2500,
-#    11 : 3750,
-#    12 : 5000,
-#    13 : 7500,
-#    14 : 10000,
-#    15 : 25000,
-#    16 : 50000,
-#    17 : 100000
+#    0 : 25,
+    1 : 50,
+#    2 : 75,
+#    3 : 100,
+#    4 : 250,
+#    5 : 500,
+#    6 : 750,
+#    7 : 1000,
+#    8 : 1250,
+#    9 : 1500,
+#    10 : 1750,
+#    11 : 2500,
+#    12 : 3750,
+#    13 : 5000,
+#    14 : 7500,
+#    15 : 10000
 }
 
 def print_board(solution):   
@@ -90,7 +93,7 @@ def splice(candidate1, candidate2):
     splice_spot = random.randrange(0,QUEENS)
     new_candidate1 = candidate1[:splice_spot] + candidate2[splice_spot:]
     new_candidate2 = candidate2[:splice_spot] + candidate1[splice_spot:]
-    return mutate(new_candidate1), mutate(new_candidate2)
+    return new_candidate1, new_candidate2
 
 def mutate(candidate):
     for c in range(0, QUEENS):
@@ -98,13 +101,27 @@ def mutate(candidate):
             candidate[c] = random.randrange(0,QUEENS)
     return candidate
 
+# def parity_check(candidate):
+#     if (sum(candidate) % 2 != ODD_OR_EVEN):
+#         index = random.randrange(0, QUEENS)
+#         candidate[index] += 1
+#         if (candidate[index] >= QUEENS):
+#             candidate[index] = ODD_OR_EVEN
+# #        candidate[index] = index
+# #        candidate = parity_check(mutate(candidate))
+#     return candidate
+
 def create_candidates(candidates):
     candidates[0], candidates[1] = splice(candidates[0], candidates[1])
+    candidates[0] = mutate(candidates[0])
+    candidates[1] = mutate(candidates[1])
+#    candidates[0] = parity_check(candidates[0])
+#    candidates[1] = parity_check(candidates[1])
     candidates.append(test_fitness(candidates[0]))
     candidates.append(test_fitness(candidates[1]))
     return candidates
 
-def generate_next_generation(scores, sum_scores, prev_best_candidate):
+def generate_next_generation(scores, sum_scores, prev_best_candidate, prev_best_score):
     next = 0
     scores_hist = {}
     best_score = 0
@@ -125,7 +142,6 @@ def generate_next_generation(scores, sum_scores, prev_best_candidate):
         candidate_list.append([list(prev_best_candidate), list(prev_best_candidate)])
     new_candidates = pool.map(create_candidates, candidate_list)
 
-#    candidate_list.append(prev_best_candidate)
     for q in range(0,int(CANDIDATE_COUNT/2)):
         new_sum_score += new_candidates[q][2]
         new_scores[tuple(new_candidates[q][0])] = new_candidates[q][2]
@@ -133,19 +149,21 @@ def generate_next_generation(scores, sum_scores, prev_best_candidate):
             best_score = new_candidates[q][2]
             best_candidate = tuple(new_candidates[q][0])
 
-            new_sum_score += new_candidates[q][3]
-            new_scores[tuple(new_candidates[q][1])] = new_candidates[q][3]
-            if (new_scores[tuple(new_candidates[q][1])] > best_score):
-                best_score = new_candidates[q][3]
-                best_candidate = tuple(new_candidates[q][1])           
+        new_sum_score += new_candidates[q][3]
+        new_scores[tuple(new_candidates[q][1])] = new_candidates[q][3]
+        if (new_scores[tuple(new_candidates[q][1])] > best_score):
+            best_score = new_candidates[q][3]
+            best_candidate = tuple(new_candidates[q][1])           
     return new_scores, best_candidate, new_sum_score
 
 def run_test():
     generation = 0
     best_fitness = 0
+    avg_fitness = []
     best_candidate = ()
     candidates = generate_initial_candidates()
     scores, best_candidate, score_sum = score_candidates(candidates)
+    avg_fitness.append(score_sum / CANDIDATE_COUNT)
 
     while(True):
         generation += 1
@@ -155,19 +173,23 @@ def run_test():
             break
         if (generation % REPORT_FREQUENCY == 0):
 #            g.write(f"{generation},{best_fitness},{best_candidate}\n")
-            print(f"Generation: {generation}   Best Fitness: {best_fitness}   Best Candidate: {best_candidate}")
-        scores, best_candidate, score_sum = generate_next_generation(scores, score_sum, best_candidate)    
-
-    #g.write(f"{generation},{best_fitness},{best_candidate}\n")
-#    print_board(best_candidate)
+            print(f"Generation: {generation}   Best Fitness: {best_fitness}   Avg Fitness: {avg_fitness[generation-1]}   Best Candidate: {best_candidate}")
+        scores, best_candidate, score_sum = generate_next_generation(scores, score_sum, best_candidate, best_fitness) 
+        avg_fitness.append(score_sum / CANDIDATE_COUNT)
+#        if (OUTPUT == True):
+#            g.write(f"{generation},{best_fitness},{avg_fitness[generation]},{best_candidate}\n")
+    if (SHOW_BOARD == True):
+        print_board(best_candidate)
     print(f"Generation: {generation}   Best Fitness: {best_fitness}  Best Candidate: {best_candidate}")
-
+#    if (OUTPUT == True):
+#        g.write(f"{generation},{best_fitness},{best_candidate}\n\n\n")
 #    f.write(f"Generation: {generation}  Solution: {best_candidate}")
     return generation
 
+if (OUTPUT == True):
+    f = open("candidates.txt", "a")
+#    g = open("generations.csv", "a")
 
-f = open("candidates.txt", "a")
-#g = open("generations.csv", "a")
 if __name__ == '__main__':
     mp.freeze_support()
     pool = mp.Pool(mp.cpu_count())
@@ -184,8 +206,9 @@ if __name__ == '__main__':
         print(f"Queens: {QUEENS}  Candidate Count: {CANDIDATE_COUNT}  Mutagen: {round(MUTAGEN, 5)}  Trials: {TRIALS}")
         print(f"Average number of generations: {round(generation_count_total/TRIALS, 3)}   Average search time: {round(search_time_total/TRIALS, 3)}")
         print()
-        f.write(f"Queens: {QUEENS}  Candidate Count: {CANDIDATE_COUNT}  Mutagen: {round(MUTAGEN, 5)}  Trials: {TRIALS}\n")
-        f.write(f"Average number of generations: {round(generation_count_total/TRIALS, 3)}   Average search time: {round(search_time_total/TRIALS, 3)}\n\n")
-
-f.close()
-#g.close()
+        if (OUTPUT == True):
+            f.write(f"Queens: {QUEENS}  Candidate Count: {CANDIDATE_COUNT}  Mutagen: {round(MUTAGEN, 5)}  Trials: {TRIALS}\n")
+            f.write(f"Average number of generations: {round(generation_count_total/TRIALS, 3)}   Average search time: {round(search_time_total/TRIALS, 3)}\n\n")
+if (OUTPUT == True):
+    f.close()
+ #   g.close()
